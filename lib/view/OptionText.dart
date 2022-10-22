a3cc4e19b6935aed5f1e628b527613274564910f
Huynh Long Ho <hohl@cafeland.local>
2022-10-22 12:07:39 +0700

    search

--- lib/view/OptionText.dart
+++ lib/view/OptionText.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class OptionText extends StatelessWidget {
+class OptionText extends StatefulWidget {
   final String title;
   final String titleSelectOption;
   final List<String> datas;
@@ -18,20 +18,25 @@ class OptionText extends StatelessWidget {
       : super(key: key);
   TextStyle styleTitle = const TextStyle(
       fontSize: 10, color: Color(0xFF666666), fontFamily: 'Montserrat');
+  @override
+  State<StatefulWidget> createState() => _optionText();
+}
 
+class _optionText extends State<OptionText> {
   @override
   Widget build(BuildContext context) {
+    List<String> searchList = widget.datas;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Visibility(
-          visible: selected.isNotEmpty,
+          visible: widget.selected.isNotEmpty,
           maintainState: true,
           maintainAnimation: true,
           maintainSize: true,
           child: Text(
-            title,
-            style: styleTitle,
+            widget.title,
+            style: widget.styleTitle,
           ),
         ),
         GestureDetector(
@@ -44,17 +49,17 @@ class OptionText extends StatelessWidget {
                   Expanded(
                       child: RichText(
                           text: TextSpan(
-                              text: selected.isNotEmpty
-                                  ? selected
-                                  : titleSelectOption,
+                              text: widget.selected.isNotEmpty
+                                  ? widget.selected
+                                  : widget.titleSelectOption,
                               style: TextStyle(
                                   fontSize: 14,
-                                  color: Color(selected.isEmpty
+                                  color: Color(widget.selected.isEmpty
                                       ? 0xFF666666
                                       : 0xFF222222),
                                   fontFamily: 'Montserrat'),
                               children: [
-                        if (isRequired && selected.isEmpty)
+                        if (widget.isRequired && widget.selected.isEmpty)
                           TextSpan(
                               text: ' *', style: TextStyle(color: Colors.red)),
                       ]))),
@@ -76,44 +81,82 @@ class OptionText extends StatelessWidget {
                 borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
               ),
               builder: (BuildContext context) {
-                return SizedBox(
+                return FractionallySizedBox(
+                    heightFactor: 0.9,
                     child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      height: 4,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: Colors.white),
-                      margin: const EdgeInsets.only(bottom: 10),
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.8,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white),
-                      child: ListView.separated(
-                        padding: const EdgeInsets.all(8),
-                        itemCount: datas.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return GestureDetector(
-                            onTap: () {
-                              selected = datas[index];
-                              Navigator.pop(context);
-                              callbackFunction(datas[index]);
-                            },
-                            child: Container(
-                                padding: const EdgeInsets.all(4),
-                                child: Text(datas[index])),
-                          );
-                        },
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const Divider(),
-                      ),
-                    )
-                  ],
-                ));
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.2,
+                          height: 4,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              color: Colors.white),
+                          margin: const EdgeInsets.only(bottom: 10),
+                        ),
+                        Container(
+                            height: MediaQuery.of(context).size.height * 0.88,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: TextField(
+                                      style:
+                                          TextStyle(color: Color(0xFF222222)),
+                                      scrollPadding: const EdgeInsets.all(4),
+                                      decoration: InputDecoration(
+                                        isDense: true,
+                                        contentPadding:
+                                            EdgeInsets.only(top: 4, bottom: 4),
+                                        labelStyle: TextStyle(
+                                            fontSize: 14.0,
+                                            color: Color(0xFF666666)),
+                                        labelText: "Tìm...",
+                                      ),
+                                      onChanged: (value) {
+                                        setState(() {
+                                          if (value.length == 0) {
+                                            searchList = widget.datas;
+                                          } else {
+                                            searchList.length = 0;
+                                            searchList = widget.datas
+                                                .where((item) =>
+                                                    item.contains(value))
+                                                .toList();
+                                          }
+                                        });
+                                      }),
+                                ),
+                                Expanded(
+                                  child: ListView.separated(
+                                    padding: const EdgeInsets.all(8),
+                                    itemCount: searchList.length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      return GestureDetector(
+                                        onTap: () {
+                                          widget.selected = searchList[index];
+                                          Navigator.pop(context);
+                                          widget.callbackFunction(
+                                              searchList[index]);
+                                        },
+                                        child: Container(
+                                            padding: const EdgeInsets.all(4),
+                                            child: Text(searchList[index])),
+                                      );
+                                    },
+                                    separatorBuilder:
+                                        (BuildContext context, int index) =>
+                                            const Divider(),
+                                  ),
+                                )
+                              ],
+                            ))
+                      ],
+                    ));
               },
             );
           },
